b2171e1665d080adf426fcdc0deb25edf80c8c00
sutralia <sutralian@gmail.com>
2021-05-09 18:50:56 +0700

    fix: bump version electron app

--- .scripts/bump-version-electron.js
+++ .scripts/bump-version-electron.js
@@ -1,22 +1,24 @@
-let fs = require('fs');
+
+const fs = require('fs');
 const { exec, execSync } = require('child_process');
 
-if (fs.existsSync('../package.json')) {
-  var package = require('../package.json');
-  let currentVersion = package.version;
+module.exports.desktop = () => {
+    if (fs.existsSync('../package.json')) {
+        let package = require('../package.json');
+        let currentVersion = package.version;
+    
+        exec('git tag --sort=committerdate | tail -1', (error, stdout) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+    
+        let newVersion = stdout.trim();
+        package.version = newVersion;
+        fs.writeFileSync('../package.json', JSON.stringify(package, null, 2));
 
-  exec('git tag --sort=committerdate | tail -1', (error, stdout) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+        let updated = require('../package.json');
+        console.log('Version updated', currentVersion, '=>', updated.version);
+        });
     }
-
-    let newVersion = stdout.trim();
-    package.version = newVersion;
-    fs.writeFileSync('../package.json', JSON.stringify(package, null, 2));
-
-    execSync("git commit -am 'Bump version '" + newVersion);
-
-    console.log('Version updated', currentVersion, '=>', newVersion);
-  });
 }
