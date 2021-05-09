21d1e860fb200e469ab6cb2da794f9f502b65b26
sutralia <sutralian@gmail.com>
2021-05-09 19:15:45 +0700

    fix: bump version electron app

--- .scripts/bump-version-electron.js
+++ .scripts/bump-version-electron.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const { exec, execSync } = require('child_process');
 
 module.exports.desktop = () => {
-    if (fs.existsSync('../package.json')) {
-        let package = require('../package.json');
+    if (fs.existsSync('./apps/desktop/src/package.json')) {
+        let package = require('../apps/desktop/src/package.json');
         let currentVersion = package.version;
     
         exec('git tag --sort=committerdate | tail -1', (error, stdout) => {
@@ -14,10 +14,37 @@ module.exports.desktop = () => {
         }
     
         let newVersion = stdout.trim();
+        if (newVersion) {
+            newVersion = newVersion.split('v')[1];
+        }
         package.version = newVersion;
-        fs.writeFileSync('../package.json', JSON.stringify(package, null, 2));
+        fs.writeFileSync('./apps/desktop/src/package.json', JSON.stringify(package, null, 2));
+    
+        let updated = require('../apps/desktop/src/package.json');
+        console.log('Version updated', currentVersion, '=>', updated.version);
+        });
+    }
+}
 
-        let updated = require('../package.json');
+module.exports.desktoptimer = () => {
+    if (fs.existsSync('./apps/desktop-timer/src/package.json')) {
+        let package = require('../apps/desktop-timer/src/package.json');
+        let currentVersion = package.version;
+    
+        exec('git tag --sort=committerdate | tail -1', (error, stdout) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+    
+        let newVersion = stdout.trim();
+        if (newVersion) {
+            newVersion = newVersion.split('v')[1];
+        }
+        package.version = newVersion;
+        fs.writeFileSync('./apps/desktop-timer/src/package.json', JSON.stringify(package, null, 2));
+    
+        let updated = require('../apps/desktop-timer/src/package.json');
         console.log('Version updated', currentVersion, '=>', updated.version);
         });
     }
